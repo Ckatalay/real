c686586be580f217eb9668957f7366f1971020c5
Çağatay Kağan Atalay <44618147+Ckatalay@users.noreply.github.com>
2024-08-18 20:38:55 +0300

    v1 i guess

--- real.py
+++ real.py
@@ -3,24 +3,29 @@ import math
 
 class Real:
     def __init__(self, diagonal, ratio):
-        self.diognal = diagonal
+        self.diagonal = diagonal
         self.ratio = ratio
         self.width_ratio, self.height_ratio = ratio.split(":")
 
     def __str__(self):
-        width, height, area = self.get_dimensions()
-        return f"{area:.2f} inches squared"
+        dimensions = self.get_dimensions()
+        return f"{dimensions['area']:.2f} inches squared"
 
     def get_dimensions(self):
         diagonal_ratio = math.sqrt(
             int(self.width_ratio) ** 2 + int(self.height_ratio) ** 2
         )
-        width = (self.diognal / diagonal_ratio) * int(self.width_ratio)
-        height = (self.diognal / diagonal_ratio) * int(self.height_ratio)
+        width = (self.diagonal / diagonal_ratio) * int(self.width_ratio)
+        height = (self.diagonal / diagonal_ratio) * int(self.height_ratio)
         area = width * height
-        return width, height, area
+        dimentions = {
+            "width": width,
+            "height": height,
+            "area": area,
+        }
+        return dimentions
 
 
 if __name__ == "__main__":
-    real = Real(55, "16:9")
+    real = Real(10, "16:9")
     print(real)
